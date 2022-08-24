caf345ea8904ddca803b502fdf7e7b3fcbe0466e
Louise Campos <louise.marcili2@gmail.com>
2022-08-23 22:47:29 -0300

    Adicionar arquivos de projeto.

---
+++ POrientacaoObjetos_Pessoa/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace POrientacaoObjetos_Pessoa
+{
+    internal class Pessoa
+    {
+        String nome;
+        String data_nasc;
+        String cpf;
+        String telefone;
+        public Pessoa(String nome, String data_nasc, String cpf, String telefone)
+        {
+            this.nome = nome;
+            this.data_nasc = data_nasc;
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+        public override string ToString()
+        {
+            return "Nome: " + nome + "\ndata de nascimento: " + data_nasc + "\ncpf: " + cpf + "\ntelefone " + telefone;
+        }
+        public String getNome()
+        {
+            return nome;
+        }
+        public String getDataNasc()
+        {
+            return data_nasc;
+        }
+        public String getCpf()
+        {
+            return cpf;
+        }
+        public String getTelefone()
+        {
+            return telefone;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ POrientacaoObjetos_Pessoa/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using POrientacaoObjetos_Pessoa;
+
+namespace PProgOrientadaObj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] Cadastro = new String[3, 4];
+            Cadastros();
+            ImprimirDados();
+            void Cadastros()
+            {
+                for (int linhas = 0; linhas < 3; linhas++)
+                {
+                    for (int coluna = 0; coluna < 4; coluna++)
+                    {
+                        switch (coluna)
+                        {
+                            case 0:
+                                Console.WriteLine("Digite o Nome");
+                                Cadastro[linhas, coluna] = Console.ReadLine();
+                                
+                                break;
+                            case 1:
+                                Console.WriteLine("Digite o data");
+                                Cadastro[linhas, coluna] = Console.ReadLine();
+                               
+                                break;
+                            case 2:
+                                Console.WriteLine("Digite o cpf");
+                                Cadastro[linhas, coluna] = Console.ReadLine();
+                               
+                                break;
+                            case 3:
+                                Console.WriteLine("Digite o telefone");
+                                Cadastro[linhas, coluna] = Console.ReadLine();
+                                Console.WriteLine();
+                                
+                                break;
+                        }
+                    }
+                }
+            }
+            void ImprimirDados()
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Pessoa pessoa = new Pessoa(Cadastro[j, 0], Cadastro[j, 1], Cadastro[j, 2], Cadastro[j, 3]);
+                    Console.WriteLine(pessoa.ToString());
+                    Console.WriteLine();
+                    
+                }   
+            }
+        }     
+    }
+}
